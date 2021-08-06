97bf0d30ca20f43a20a7abfdd14754129358dbc7
Braulio <braulio007@gmail.com>
2021-08-06 16:19:31 -0300

    Update tabuada.py

--- tabuada.py
+++ tabuada.py
@@ -4,6 +4,8 @@ N = int(input())
 for i in range(1, 11):
     print ("{} * {} = {}". format(N, i, N * i))
 
+###########################################################
+    
 #e.:2
 N = int(input())
 for i in range(1, 11):
