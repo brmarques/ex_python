72fff6d439c39c638503558d34398e89a0a6a849
Braulio <braulio007@gmail.com>
2021-08-06 16:18:46 -0300

    create tabuada.py
    create tabuada.py

---
+++ tabuada.py
@@ -0,0 +1,10 @@
+#única diferença entre eles é a sintaxe da saída (print)
+#ex.:1
+N = int(input())
+for i in range(1, 11):
+    print ("{} * {} = {}". format(N, i, N * i))
+
+#e.:2
+N = int(input())
+for i in range(1, 11):
+    print (f"{N} * {i} = {N * i}")
